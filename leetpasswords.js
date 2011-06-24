868323dc2f2f103fb54347979f2e2a5b62c43fb2
rozuur <rozuur@gmail.com>
2011-06-24 19:38:33 +0530

    seems like for in loop doesn't work for strings, size parameter added for generating more passwords.

--- leetpasswords.js
+++ leetpasswords.js
@@ -1,24 +1,32 @@
 function generatePasswords(){
     var hint = document.getElementsByName('hint')[0].value;
-    var passwds = leetPasswords(hint);
-    alert(passwds);
+    var passwds = leetPasswords(hint,10);
+    alert("Password is " + passwds);
 }
-                                                 
 
-function leetPasswords(hint){
+function leetPasswords(hint, size){
+    var results = [];
+    for(var i = 0; i < size; ++i){
+        results.push(leetPassword(hint));
+    }
+    return results;
+}
+
+function leetPassword(hint){
     var mappings = new Array();
     mappings['A'] = new Array("4","/-\\","@","^","/\\","//-\\\\","/=\\");
     mappings['E'] = new Array("3","ii","(=");
     var results = [];
     var upped = hint.toUpperCase();
-    for(c in upped){
+    for(var i = 0; i < upped.length; ++i){
+        var c = upped.charAt(i);
         results.push(mappings[c].randElem());
     }
     return results.join(' ');
 }
 
 Array.prototype.randElem = function(){
-  return this[getRandomInt(0,this.length - 1)];  
+  return this[getRandomInt(0,this.length - 1)];
 };
 
 // Returns a random integer between min and max
