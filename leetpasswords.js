33bddf614620ca79e99d71ba87798490a965e3d5
rozuur <rozuur@gmail.com>
2011-06-24 18:56:46 +0530

    A simple project to generate passwords based on leet code.

---
+++ leetpasswords.js
@@ -0,0 +1,10 @@
+function generatePasswords(){
+    var hint = document.getElementsByName('hint')[0].value;
+    var passwds = leetPasswords(hint);
+    alert(passwds[0] + "|" + passwds[1]);
+}
+                                                 
+
+function leetPasswords(hint){
+    
+}
