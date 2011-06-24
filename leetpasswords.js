a88bcb289f30717f055c1772c316075535c74295
rozuur <rozuur@gmail.com>
2011-06-24 19:26:09 +0530

    added a prototype for array to return a random element form it and associated array is used to store mappings between character and leet replacement.

--- leetpasswords.js
+++ leetpasswords.js
@@ -1,10 +1,30 @@
 function generatePasswords(){
     var hint = document.getElementsByName('hint')[0].value;
     var passwds = leetPasswords(hint);
-    alert(passwds[0] + "|" + passwds[1]);
+    alert(passwds);
 }
                                                  
 
 function leetPasswords(hint){
-    
+    var mappings = new Array();
+    mappings['A'] = new Array("4","/-\\","@","^","/\\","//-\\\\","/=\\");
+    mappings['E'] = new Array("3","ii","(=");
+    var results = [];
+    var upped = hint.toUpperCase();
+    for(c in upped){
+        results.push(mappings[c].randElem());
+    }
+    return results.join(' ');
+}
+
+Array.prototype.randElem = function(){
+  return this[getRandomInt(0,this.length - 1)];  
+};
+
+// Returns a random integer between min and max
+// Using Math.round() will give you a non-uniform distribution!
+// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
